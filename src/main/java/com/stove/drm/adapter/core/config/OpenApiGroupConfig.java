f25d9f22be5a638e04e8f44481982d2f36966d28
devkim <gate7711@gmail.com>
2025-11-26 16:07:57 +0900

    스웨거 서버 추가.

--- src/main/java/com/stove/drm/adapter/core/config/OpenApiGroupConfig.java
+++ src/main/java/com/stove/drm/adapter/core/config/OpenApiGroupConfig.java
@@ -1,8 +1,11 @@
 package com.stove.drm.adapter.core.config;
 
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiGroupConfig {
@@ -12,7 +15,14 @@ public class OpenApiGroupConfig {
         return GroupedOpenApi.builder()
                 .group("v1")
                 .pathsToMatch("/v1/**")
-                .build();
+                .addOpenApiCustomizer(openApi -> {
+                    openApi.setServers( List.of(
+                            new Server().url("https://dooray-drm.spay.sginfra.net")
+                            ,new Server().url("http://localhost:9090")
+                            ,new Server().url("http://172.26.102.72:9090")
+                            )
+                    );
+                }).build();
     }
 
 //    @Bean
